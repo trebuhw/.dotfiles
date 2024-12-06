f14eff13cb6fd5f1d5ef2caf5834b2e8a2ba0467
trebuhw <114191788+trebuhw@users.noreply.github.com>
2024-12-06 17:49:08 +0100

    edit waybar

--- hypr/.config/hypr/scripts/rofi-power-menu.sh
+++ hypr/.config/hypr/scripts/rofi-power-menu.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-chosen=$(printf " Lock\n󰍃 Logout\n󰛧 Screenoff\n󰈆 Suspend\n Reboot\n󰐥 Poweroff" | rofi -dmenu -i -p " ")
+chosen=$(printf " Lock\n󰍃 Logout\n󰛧 ScreenOff\n󰈆 Suspend\n Reboot\n󰐥 PowerOff" | rofi -dmenu -i -p " ")
 
 case "$chosen" in
 	" Lock") ~/.config/hypr/scripts/lock.sh;;
 	"󰍃 Logout") killall Hyprland;;
-	"󰛧 Screenoff") ~/.config/hypr/scripts/offscreen.sh;;
+	"󰛧 ScreenOff") ~/.config/hypr/scripts/offscreen.sh;;
 	"󰈆 Suspend") systemctl suspend ;;
 	" Reboot") systemctl reboot ;;
-	"󰐥 Poweroff") systemctl poweroff;;
+	"󰐥 PowerOff") systemctl poweroff;;
 	*) exit 1 ;;
 esac
 
