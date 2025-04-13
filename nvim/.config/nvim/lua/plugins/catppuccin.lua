194f1fedbb34039859654d656a4a02ae04cb3cfa
trebuhw <114191788+trebuhw@users.noreply.github.com>
2025-04-13 23:24:53 +0200

    add themes

--- nvim/.config/nvim/lua/plugins/catppuccin.lua
+++ nvim/.config/nvim/lua/plugins/catppuccin.lua
@@ -7,7 +7,7 @@ return {
   
       config = function()
         require("catppuccin").setup({
-          transparent_background = false,
+          transparent_background = true,
         })
         vim.cmd.colorscheme "catppuccin-mocha"
       end
