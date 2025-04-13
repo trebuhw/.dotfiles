8cd1a09dce7fbf02b198b8b85d15a0d7d1634414
trebuhw <114191788+trebuhw@users.noreply.github.com>
2025-04-13 15:57:02 +0200

    astronvim 5

---
+++ suckless/.config/suckless/scripts/dmenu-notes.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+folder=$HOME/notes/
+
+newnote () { \
+  name="$(echo "" | dmenu -c -sb "#a3be8c" -nf "#d8dee9" -p "Enter a name: " <&-)"
+  setsid -f "$TERMINAL" -e nvim $folder$name".md" >/dev/null 2>&1
+}
+
+selected () { \
+  choice=$(echo -e "New\n$(command ls -t1 $folder)" | dmenu -c -l 5 -i -p "Choose note or create new: ")
+  case $choice in
+    New) newnote ;;
+    *.md) setsid -f "$TERMINAL" -e nvim "$folder$choice" >/dev/null 2>&1 ;;
+    *) exit ;;
+  esac
+}
+
+selected
