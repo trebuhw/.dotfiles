1e5488cc2b85e7855901d85a7e23cf07ee90081f
trebuhw <114191788+trebuhw@users.noreply.github.com>
2025-04-21 18:46:38 +0200

    up dwm

--- suckless/.config/suckless/dwm/config.def.h
+++ suckless/.config/suckless/dwm/config.def.h
@@ -70,7 +70,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-i", NULL };
 /*static const char *dmenucmd[] = { "dmenu_run", "-p", prompt, "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };*/
-static const char *terminalcmd[]  = { "kitty", NULL };
+static const char *terminalcmd[]  = { "wezterm", NULL };
 static const char *filecmd[]  = { "thunar", NULL };
 static const char *webbrowser[]  = { "firefox", NULL };
 
