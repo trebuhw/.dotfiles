4e36d9b5c85ef24f60cdd6c3a75dc7ce0c219380
trebuhw <114191788+trebuhw@users.noreply.github.com>
2025-05-07 00:21:15 +0200

    add rclone.sh

--- suckless/.config/suckless/scripts/rclone.sh
+++ suckless/.config/suckless/scripts/rclone.sh
@@ -2,6 +2,6 @@
 
 echo "Starting rclone"
 rclone --vfs-cache-mode writes mount gdrive: ~/Cloud/GDrive &
-# rclone --vfs-cache-mode writes mount onedrive: ~/Cloud/OneDrive &
+rclone --vfs-cache-mode writes mount onedrive: ~/Cloud/OneDrive &
 
 exit 0
