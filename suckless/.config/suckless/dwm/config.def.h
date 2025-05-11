99b0311633b529f09001111eb0a2d11ea48758be
trebuhw <114191788+trebuhw@users.noreply.github.com>
2025-05-11 14:48:23 +0200

    up

--- suckless/.config/suckless/dwm/config.def.h
+++ suckless/.config/suckless/dwm/config.def.h
@@ -12,8 +12,8 @@ static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const Bool viewontag         = True;     /* Switch view on tag switch */
-static const char *fonts[]          = { "JetBrainsMono Nerd Font:size=11" };
-static const char dmenufont[]       = "JetBrainsMono Nerd Font:size=11";
+static const char *fonts[]          = { "JetBrainsMono Nerd Font:size=10" };
+static const char dmenufont[]       = "JetBrainsMono Nerd Font:size=10";
 static const char col_gray1[]       = "#1e1e2e";
 static const char col_gray2[]       = "#45475a";
 static const char col_gray3[]       = "#cdd6f4";
