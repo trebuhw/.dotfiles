c1e32f54337025adbe382b4ec993da1690c52f0f
trebuhw <114191788+trebuhw@users.noreply.github.com>
2025-07-30 21:22:12 +0200

    up

--- fish/.config/fish/alias.fish
+++ fish/.config/fish/alias.fish
@@ -252,7 +252,7 @@ alias xsz='./xbps-src zap' # czyszczenie wraz z zależnościami
 # Google-Chrome > install & update
 alias igch='~/.local/bin/src-install_chrome.sh'
 alias upgch='~/.local/bin/void_chrome_install_update.sh'
-alias upgch='~/.local/bin/void_ghostty_install_update.sh'
+alias upgho='~/.local/bin/void_ghostty_install_update.sh'
 
 # Power
 alias vrb='loginctl reboot' # Potrzeba zainstalować i uruchomić usługę logind
