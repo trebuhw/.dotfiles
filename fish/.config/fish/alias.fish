49ae52f93e23034a0d1ea44df2bae0a7097beb34
trebuhw <114191788+trebuhw@users.noreply.github.com>
2025-08-07 13:49:40 +0200

    up

--- fish/.config/fish/alias.fish
+++ fish/.config/fish/alias.fish
@@ -116,8 +116,8 @@ alias sysfailed="systemctl list-units --failed"
 alias nv='nvim'
 alias mi='/home/hubert/.config/suckless/scripts/make-install.sh'
 alias fzf='fzf --preview "bat --color=always {}"'
-alias fe='$HOME/.local/bin/fzf-nvim.sh'
-alias fel='$HOME/.local/bin/fzf-nvim.sh $(pwd)/'
+alias fe='$HOME/.local/share/bin/fzf-nvim.sh'
+alias fel='$HOME/.local/share/bin/fzf-nvim.sh $(pwd)/'
 alias ffcp='cliphist list | fzf | cliphist decode | wl-copy'
 alias ddm='if=/dev/sr0 of=cd.iso bs=4M status=progress' # cd.iso tworzy się w bierzącym katalogu
 alias hwi='hyprprop' # hyprland wayland, info run program
