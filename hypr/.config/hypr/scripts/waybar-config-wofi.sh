da427d5ce173bca94039ed33e110cad450ddf849
trebuhw <114191788+trebuhw@users.noreply.github.com>
2025-08-16 18:15:57 +0200

    up

--- hypr/.config/hypr/scripts/waybar-config-wofi.sh
+++ hypr/.config/hypr/scripts/waybar-config-wofi.sh
@@ -22,75 +22,75 @@ wofi_command="wofi --show dmenu \
 			--hide-scroll --no-actions \
 			--matching=fuzzy"
 
-
-menu(){
-printf "1. All\n" 
-printf "2. Color\n" 
-printf "3. Color-Icon\n" 
-printf "4. Default\n"
-printf "5. Dual\n"
-printf "6. Gnome\n"
-printf "7. Plasma\n"
-printf "8. Simple\n"
-printf "9. Omarchy\n"
+menu() {
+  printf "1. All\n"
+  printf "2. Color\n"
+  printf "3. Color-Icon\n"
+  printf "4. Default\n"
+  printf "5. Dual\n"
+  printf "6. Gnome\n"
+  printf "7. Plasma\n"
+  printf "8. Simple\n"
+  printf "9. Omarchy\n"
+  printf "10. i3\n"
 }
 
 main() {
-    choice=$(menu | ${wofi_command} | cut -d. -f1)
-    case $choice in
-        1)
-            ln -sf "$HOME/.config/waybar/configs/all-config-hypr" "$WAYBARCONFIG"
-            ;;
-        2)
-            ln -sf "$HOME/.config/waybar/configs/color.config-hypr" "$WAYBARCONFIG"
-            ;;
-        3)
-            ln -sf "$HOME/.config/waybar/configs/color-icon-config-hypr" "$WAYBARCONFIG"
-            ;;
-        4)
-            ln -sf "$HOME/.config/waybar/configs/config-default" "$WAYBARCONFIG"
-            ;;
-        5)
-            ln -sf "$HOME/.config/waybar/configs/config-dual" "$WAYBARCONFIG"
-            ;;
-	    6)
-            ln -sf "$HOME/.config/waybar/configs/config-gnome" "$WAYBARCONFIG"
-            ;;
-        7)    
-            ln -sf "$HOME/.config/waybar/configs/config-plasma" "$WAYBARCONFIG"
-            ;;
-        8)
-             ln -sf "$HOME/.config/waybar/configs/config-simple" "$WAYBARCONFIG"
-             ;;
-        9)    
-             ln -sf "$HOME/.config/waybar/configs/omarchy.jsonc" "$WAYBARCONFIG"
-             ;;
-        10)    
+  choice=$(menu | ${wofi_command} | cut -d. -f1)
+  case $choice in
+  1)
+    ln -sf "$HOME/.config/waybar/configs/all-config-hypr" "$WAYBARCONFIG"
+    ;;
+  2)
+    ln -sf "$HOME/.config/waybar/configs/color.config-hypr" "$WAYBARCONFIG"
+    ;;
+  3)
+    ln -sf "$HOME/.config/waybar/configs/color-icon-config-hypr" "$WAYBARCONFIG"
+    ;;
+  4)
+    ln -sf "$HOME/.config/waybar/configs/config-default" "$WAYBARCONFIG"
+    ;;
+  5)
+    ln -sf "$HOME/.config/waybar/configs/config-dual" "$WAYBARCONFIG"
+    ;;
+  6)
+    ln -sf "$HOME/.config/waybar/configs/config-gnome" "$WAYBARCONFIG"
+    ;;
+  7)
+    ln -sf "$HOME/.config/waybar/configs/config-plasma" "$WAYBARCONFIG"
+    ;;
+  8)
+    ln -sf "$HOME/.config/waybar/configs/config-simple" "$WAYBARCONFIG"
+    ;;
+  9)
+    ln -sf "$HOME/.config/waybar/configs/omarchy.jsonc" "$WAYBARCONFIG"
+    ;;
+  10)
+    ln -sf "$HOME/.config/waybar/configs/i3-config.json" "$WAYBARCONFIG"
+    ;;
+  11)
 
-
-             if pgrep -x "waybar" >/dev/null; then
-                killall waybar
-                exit
-            fi
-            ;;        
-        *)
-            ;;
-    esac
+    if pgrep -x "waybar" >/dev/null; then
+      killall waybar
+      exit
+    fi
+    ;;
+  *) ;;
+  esac
 }
 
 # Check if wofi is already running
 if pidof wofi >/dev/null; then
-    killall wofi
-    exit 0
+  killall wofi
+  exit 0
 else
-    main
+  main
 fi
 
 # Restart Waybar and run other scripts if a choice was made
 if [[ -n "$choice" ]]; then
-    # Restart Waybar
-    killall waybar
+  # Restart Waybar
+  killall waybar
 fi
 
 exec ~/.config/hypr/scripts/WaybarRestart.sh &
-                
--- hypr/.config/hypr/scripts/waybar-styles-rofi.sh
+++ hypr/.config/hypr/scripts/waybar-styles-rofi.sh
@@ -22,82 +22,83 @@ wofi_command="wofi --show dmenu \
 			--hide-scroll --no-actions \
 			--matching=fuzzy"
 
-
-menu(){
-printf "1. Color\n" 
-printf "2. Black\n" 
-printf "3. None\n"
-printf "4. B&W\n"
-printf "5. Light\n"
-printf "6. Mauve\n"
-printf "7. RGB\n"
-printf "8. Simple\n"
-printf "9. Dark\n"
-printf "10. Default\n"
-printf "11. Omarchy\n"
+menu() {
+  printf "1. Color\n"
+  printf "2. Black\n"
+  printf "3. None\n"
+  printf "4. B&W\n"
+  printf "5. Light\n"
+  printf "6. Mauve\n"
+  printf "7. RGB\n"
+  printf "8. Simple\n"
+  printf "9. Dark\n"
+  printf "10. Default\n"
+  printf "11. Omarchy\n"
+  printf "12. i3\n"
 }
 
 main() {
-    choice=$(menu | ${wofi_command} | cut -d. -f1)
-    case $choice in
-        1)
-            ln -sf "$HOME/.config/waybar/style/color.style.css" "$WAYBARSTYLE" && ln -sf "$HOME/.config/waybar/configs/color.config-hypr" "$WAYBARCONFIG"
-            ;; 
-        2)
-            ln -sf "$HOME/.config/waybar/style/black.style.css" "$WAYBARSTYLE" && ln -sf "$HOME/.config/waybar/configs/all-config-hypr" "$WAYBARCONFIG"
-            ;;
-        3)
-            ln -sf "$HOME/.config/waybar/style/none.style.css" "$WAYBARSTYLE" && ln -sf "$HOME/.config/waybar/configs/all-config-hypr" "$WAYBARCONFIG"
-            ;;
-        4)
-            ln -sf "$HOME/.config/waybar/style/style-b&w.css" "$WAYBARSTYLE" && ln -sf "$HOME/.config/waybar/configs/all-config-hypr" "$WAYBARCONFIG"
-            ;;
-        5)
-            ln -sf "$HOME/.config/waybar/style/style-light.css" "$WAYBARSTYLE" && ln -sf "$HOME/.config/waybar/configs/all-config-hypr" "$WAYBARCONFIG"
-            ;;
-	    6)
-            ln -sf "$HOME/.config/waybar/style/style-mauve.css" "$WAYBARSTYLE" && ln -sf "$HOME/.config/waybar/configs/all-config-hypr" "$WAYBARCONFIG"
-            ;;
-        7)    
-             ln -sf "$HOME/.config/waybar/style/style-rgb.css" "$WAYBARSTYLE" && ln -sf "$HOME/.config/waybar/configs/all-config-hypr" "$WAYBARCONFIG"
-            ;;
-        8)
-             ln -sf "$HOME/.config/waybar/style/style-simple.css" "$WAYBARSTYLE" && ln -sf "$HOME/.config/waybar/configs/all-config-hypr" "$WAYBARCONFIG"
-             ;;
-        9)     
-             ln -sf "$HOME/.config/waybar/style/style-dark.css" "$WAYBARSTYLE" && ln -sf "$HOME/.config/waybar/configs/all-config-hypr" "$WAYBARCONFIG"
-             ;;
-        10)
-             ln -sf "$HOME/.config/waybar/style/style-default.css" "$WAYBARSTYLE" && ln -sf "$HOME/.config/waybar/configs/all-config-hypr" "$WAYBARCONFIG"
-             ;;
-        11)
-             ln -sf "$HOME/.config/waybar/style/omarchy-style.css" "$WAYBARSTYLE" && ln -sf "$HOME/.config/waybar/configs/all-config-hypr" "$WAYBARCONFIG"
-             ;;
-        12)
+  choice=$(menu | ${wofi_command} | cut -d. -f1)
+  case $choice in
+  1)
+    ln -sf "$HOME/.config/waybar/style/color.style.css" "$WAYBARSTYLE" && ln -sf "$HOME/.config/waybar/configs/color.config-hypr" "$WAYBARCONFIG"
+    ;;
+  2)
+    ln -sf "$HOME/.config/waybar/style/black.style.css" "$WAYBARSTYLE" && ln -sf "$HOME/.config/waybar/configs/all-config-hypr" "$WAYBARCONFIG"
+    ;;
+  3)
+    ln -sf "$HOME/.config/waybar/style/none.style.css" "$WAYBARSTYLE" && ln -sf "$HOME/.config/waybar/configs/all-config-hypr" "$WAYBARCONFIG"
+    ;;
+  4)
+    ln -sf "$HOME/.config/waybar/style/style-b&w.css" "$WAYBARSTYLE" && ln -sf "$HOME/.config/waybar/configs/all-config-hypr" "$WAYBARCONFIG"
+    ;;
+  5)
+    ln -sf "$HOME/.config/waybar/style/style-light.css" "$WAYBARSTYLE" && ln -sf "$HOME/.config/waybar/configs/all-config-hypr" "$WAYBARCONFIG"
+    ;;
+  6)
+    ln -sf "$HOME/.config/waybar/style/style-mauve.css" "$WAYBARSTYLE" && ln -sf "$HOME/.config/waybar/configs/all-config-hypr" "$WAYBARCONFIG"
+    ;;
+  7)
+    ln -sf "$HOME/.config/waybar/style/style-rgb.css" "$WAYBARSTYLE" && ln -sf "$HOME/.config/waybar/configs/all-config-hypr" "$WAYBARCONFIG"
+    ;;
+  8)
+    ln -sf "$HOME/.config/waybar/style/style-simple.css" "$WAYBARSTYLE" && ln -sf "$HOME/.config/waybar/configs/all-config-hypr" "$WAYBARCONFIG"
+    ;;
+  9)
+    ln -sf "$HOME/.config/waybar/style/style-dark.css" "$WAYBARSTYLE" && ln -sf "$HOME/.config/waybar/configs/all-config-hypr" "$WAYBARCONFIG"
+    ;;
+  10)
+    ln -sf "$HOME/.config/waybar/style/style-default.css" "$WAYBARSTYLE" && ln -sf "$HOME/.config/waybar/configs/all-config-hypr" "$WAYBARCONFIG"
+    ;;
+  11)
+    ln -sf "$HOME/.config/waybar/style/omarchy-style.css" "$WAYBARSTYLE" && ln -sf "$HOME/.config/waybar/configs/all-config-hypr" "$WAYBARCONFIG"
+    ;;
+  12)
+    ln -sf "$HOME/.config/waybar/style/i3-style.json" "$WAYBARSTYLE" && ln -sf "$HOME/.config/waybar/configs/all-config-hypr" "$WAYBARCONFIG"
+    ;;
+  13)
 
-             if pgrep -x "waybar" >/dev/null; then
-                killall waybar
-                exit
-            fi
-            ;;        
-        *)
-            ;;
-    esac
+    if pgrep -x "waybar" >/dev/null; then
+      killall waybar
+      exit
+    fi
+    ;;
+  *) ;;
+  esac
 }
 
 # Check if wofi is already running
 if pidof wofi >/dev/null; then
-    killall wofi
-    exit 0
+  killall wofi
+  exit 0
 else
-    main
+  main
 fi
 
 # Restart Waybar and run other scripts if a choice was made
 if [[ -n "$choice" ]]; then
-    # Restart Waybar
-    killall waybar
+  # Restart Waybar
+  killall waybar
 fi
 
 exec ~/.config/hypr/scripts/WaybarRestart.sh &
-                
