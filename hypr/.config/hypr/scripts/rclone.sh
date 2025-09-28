cd7fc8eba6e1fddbd335c3b8861cc2ea5efee452
trebuhw <114191788+trebuhw@users.noreply.github.com>
2025-09-28 21:00:20 +0200

    up

--- hypr/.config/hypr/scripts/rclone.sh
+++ hypr/.config/hypr/scripts/rclone.sh
@@ -4,5 +4,6 @@ echo "Starting rclone"
 rclone --vfs-cache-mode writes mount gdrive: ~/Cloud/GDrive &
 rclone --vfs-cache-mode writes mount onedrive: ~/Cloud/OneDrive &
 rclone --vfs-cache-mode writes mount pcloud: ~/Cloud/PCloud &
+# rclone --vfs-cache-mode writes mount proton: ~/Cloud/ProtonDrive &
 
 exit 0
